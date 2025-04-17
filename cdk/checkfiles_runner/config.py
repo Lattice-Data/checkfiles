2190d9e985657181f150076a91a00a4cd74f9307
Idan <gabdank@stanford.edu>
2025-04-17 11:55:35 -0700

    update config slack arn names

--- cdk/checkfiles_runner/config.py
+++ cdk/checkfiles_runner/config.py
@@ -3,8 +3,8 @@ config = {
     'account_production': '585222078325',  # lattice-prod
     'ami_id_production': 'ami-0c74fc746d4803d21', # latest checkfiles AMI
     'portal_secrets_arn_production': 'arn:aws:secretsmanager:us-west-1:585222078325:secret:checkfiles-portal-secret-5A1cNL',
-    'slack_channel_id': 'arn:aws:secretsmanager:us-west-1:585222078325:secret:slack-channel-id-lWBvOZ',
-    'slack_token': 'arn:aws:secretsmanager:us-west-1:585222078325:secret:bot-token-Z0jak9',
+    'slack_channel_id_arn': 'arn:aws:secretsmanager:us-west-1:585222078325:secret:slack-channel-id-lWBvOZ',
+    'slack_token_arn': 'arn:aws:secretsmanager:us-west-1:585222078325:secret:bot-token-Z0jak9',
     'instance_name_production': 'checkfiles',
     'instance_profile_arn_production': 'arn:aws:iam::585222078325:instance-profile/checkfiles-instance',
     'instance_security_group_production': 'sg-0da14ac5025210cf9',
